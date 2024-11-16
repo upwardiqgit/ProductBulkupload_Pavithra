ba98c38c76d0364018dde5ecec2556bc1f026e5a
BondigaPavithra <139877382+BondigaPavithra@users.noreply.github.com>
2024-11-16 22:12:22 +0530

    Add files via upload

---
+++ ProductBulkupload_Pavithra/ProductBulkUpload/src/main/java/com/Upwardiq/Pavithra/ProductBulkUploadApplication.java
@@ -0,0 +1,14 @@
+package com.Upwardiq.Pavithra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductBulkUploadApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		SpringApplication.run(ProductBulkUploadApplication.class, args);
+	}
+
+}
