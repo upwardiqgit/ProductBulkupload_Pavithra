eedf3cc3b37eb7c6adcd3709e1f973ee14b317b1
BondigaPavithra <139877382+BondigaPavithra@users.noreply.github.com>
2024-11-16 20:12:46 +0530

    first commit

---
+++ UpwardIQProductBulkUpload/ProductBulkUpload/src/main/java/com/Upwardiq/Pavithra/UpwardProductBulkUploadApplication.java
@@ -0,0 +1,13 @@
+package com.Upwardiq.Pavithra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpwardProductBulkUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpwardProductBulkUploadApplication.class, args);
+	}
+
+}
---
+++ UpwardIQProductBulkUpload/ProductBulkUpload/src/test/java/com/Upwardiq/Pavithra/ProductBulkUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Upwardiq.Pavithra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductBulkUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
